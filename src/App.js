1982ff66991409ba4461770faa9e96122ad3c424
Muhammad Faheem Akhtar <muhammadfaheemakhtar.pk@gmail.com>
2019-07-21 12:37:33 +0500

    Configured MDC

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from "@material/react-button";
+import React, { Component } from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button
+          raised
+          className="button-alternate"
+          onClick={() => console.log("clicked!")}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Click Me!
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
