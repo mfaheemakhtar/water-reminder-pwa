c0926fa14e57ddfd456cb78f5d055b47be8959bc
Muhammad Faheem Akhtar <muhammadfaheemakhtar.pk@gmail.com>
2019-07-22 22:53:40 +0500

    Add menu list

---
+++ src/constants/menu.js
@@ -0,0 +1,23 @@
+const MENU = [
+  {
+    icon: "edit",
+    text: "Glass / Cup"
+  },
+
+  {
+    icon: "today",
+    text: "Goal"
+  },
+
+  {
+    icon: "settings",
+    text: "Settings"
+  },
+
+  {
+    icon: "info",
+    text: "About"
+  }
+];
+
+export default MENU;
