fe525f1b812ad6d65448750829ba04f462f62ea3
Muhammad Faheem Akhtar <muhammadfaheemakhtar.pk@gmail.com>
2019-07-23 22:13:46 +0500

    Add PieChart and text on the screen

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import { TopAppBarFixedAdjust } from "@material/react-top-app-bar";
 import React, { Component } from "react";
 import "./App.scss";
 import Drawer from "./components/Drawer";
+import { Cell, Grid, Row } from "./components/Layout";
 import List from "./components/List";
+import PieChart from "./components/PieChart";
 import TopNavBar from "./components/TopNavBar";
+import { Body1 } from "./components/Typography";
 import MENU from "./constants/menu";
 
 class App extends Component {
@@ -27,7 +30,25 @@ class App extends Component {
 
         <TopNavBar onMenuClick={this.toggleMenu} title="Water Reminder App" />
 
-        <TopAppBarFixedAdjust>App content here</TopAppBarFixedAdjust>
+        <TopAppBarFixedAdjust>
+          <Grid>
+            <Row>
+              <Cell desktopColumns={6} phoneColumns={2} tabletColumns={4}>
+                <PieChart color="#6200ee" value={80} />
+              </Cell>
+
+              <Cell
+                align="middle"
+                desktopColumns={6}
+                phoneColumns={2}
+                tabletColumns={4}
+              >
+                <Body1 className="mb-0 text-2rem text-primary">1000 mL</Body1>
+                <Body1>out of 1800 mL</Body1>
+              </Cell>
+            </Row>
+          </Grid>
+        </TopAppBarFixedAdjust>
       </div>
     );
   }
