7b522f21faf0de1b7f76ca8d3021ba1fced88e57
Muhammad Faheem Akhtar <muhammadfaheemakhtar.pk@gmail.com>
2019-07-28 23:00:21 +0500

    Add glass types

---
+++ src/constants/glass.js
@@ -0,0 +1,28 @@
+const GLASS_SIZES = [
+  {
+    icon: "add_circle",
+    quantity: 100
+  },
+
+  {
+    icon: "add_circle",
+    quantity: 200
+  },
+
+  {
+    icon: "add_circle",
+    quantity: 250
+  },
+
+  {
+    icon: "add_circle",
+    quantity: 300
+  },
+
+  {
+    icon: "add_circle",
+    quantity: 500
+  }
+];
+
+export default GLASS_SIZES;
